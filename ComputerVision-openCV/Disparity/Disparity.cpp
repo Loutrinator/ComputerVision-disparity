ec0bb4fc87ae7b2544376792997fceff698a7230
Antoine Pavy <antoine.pavy92@gmail.com>
2021-11-24 22:11:36 +0100

    First image read

--- ComputerVision-openCV/Disparity/Disparity.cpp
+++ ComputerVision-openCV/Disparity/Disparity.cpp
@@ -1,20 +1,34 @@
 // Disparity.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+#include <opencv2/opencv.hpp>
 
 #include <iostream>
+#include <string>
 
-int main()
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv)
 {
-    std::cout << "Hello World!\n";
+	// Read the image file
+	Mat image = imread("./resources/lebeaugosse.png");
+
+	if (image.empty()) // Check for failure
+	{
+		cout << "Could not open or find the image" << endl;
+		system("pause"); //wait for any key press
+		return -1;
+	}
+
+	String windowName = "My HelloWorld Window"; //Name of the window
+
+	namedWindow(windowName); // Create a window
+
+	imshow(windowName, image); // Show our image inside the created window.
+
+	waitKey(0); // Wait for any keystroke in the window
+
+	destroyWindow(windowName); //destroy the created window
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
