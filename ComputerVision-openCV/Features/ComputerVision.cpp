81ec583e3d66a016d69b41e2907c8fd27f263c89
Antoine Pavy <antoine.pavy92@gmail.com>
2021-12-07 00:09:54 +0100

    WIP display matching points
    (crashes after a few frames)

--- ComputerVision-openCV/Features/ComputerVision.cpp
+++ ComputerVision-openCV/Features/ComputerVision.cpp
@@ -2,14 +2,16 @@
 
 using namespace cv;
 
-ComputerVision::ComputerVision() : lineColor(0,0,255), pointColor(0, 255, 255), roiColor(0, 255, 0), distanceValue(20)
+ComputerVision::ComputerVision() : lineColor(0,0,255), pointColor(0, 255, 255), roiColor(0, 255, 0), distanceValue(0.65)
 {
 }
 
 void ComputerVision::drawWindow(cv::Mat* inputFrame, cv::Mat* targetFrame, std::string windowName)
 {
 	cv::Mat iFrame = inputFrame->clone();
-	cv::Mat tFrame = targetFrame->clone();;
+	cv::Mat tFrame = targetFrame->clone();
+
+	/*
 	std::vector<Point2f> lPoints;
 	cv::KeyPoint::convert(lKeyPts, lPoints);
 	std::vector<Point2f> rPoints;
@@ -23,23 +25,26 @@ void ComputerVision::drawWindow(cv::Mat* inputFrame, cv::Mat* targetFrame, std::
 		cv::Point2f point = rPoints[i];
 		cv::circle(tFrame, point, 3, pointColor, 1);
 	}
-	cv::imshow("video", iFrame);
+	cv::imshow(windowName, iFrame);
 	cv::imshow("object", tFrame);
-	
+	*/
+
 	//std::vector<DMatch> good;
-	//cv::Mat resultImage;
-    //cv:drawMatches(*targetFrame, rKeyPts, *inputFrame, lKeyPts, validFeatures, resultImage);
-
-	//cv::imshow("resultImage", resultImage);
-
+	cv::Mat resultImage;
+	std::cout << "lKeyPts : " << lKeyPts.size() << " rKeyPts : " << rKeyPts.size() << std::endl;
+	std::cout << "targetFrame : " << (*targetFrame).size() << " inputFrame : " << (*inputFrame).size() << std::endl;
+	std::cout << "validFeatures : " << validFeatures.size() << std::endl;
+	cv::drawMatches(*targetFrame, lKeyPts, *inputFrame, rKeyPts, validFeatures, resultImage);
+	cv::imshow(windowName, resultImage);
 }
 
 void ComputerVision::detectComputePoints(cv::Ptr<cv::ORB> orb, cv::Mat* video, cv::Mat* target)
 {
-	orb->detect(*video, lKeyPts);
-	orb->compute(*video, lKeyPts, leftDesc);
-	orb->detect(*target, rKeyPts);
-	orb->compute(*target, rKeyPts, rightDesc);
+
+	orb->detect(*target, lKeyPts);
+	orb->compute(*target, lKeyPts, leftDesc);
+	orb->detect(*video, rKeyPts);
+	orb->compute(*video, rKeyPts, rightDesc);
 
 	cv::Ptr <cv::BFMatcher> bfmMatcher = cv::BFMatcher::create();
 	std::vector<std::vector<cv::DMatch>> matrixOfMatches;
@@ -51,7 +56,5 @@ void ComputerVision::detectComputePoints(cv::Ptr<cv::ORB> orb, cv::Mat* video, c
 			validFeatures.push_back(match[0]);
 		}
 	}
-
-
 }
 
--- ComputerVision-openCV/Features/Features.cpp
+++ ComputerVision-openCV/Features/Features.cpp
@@ -12,14 +12,17 @@ using namespace std;
 
 ComputerVision CVengine;
 
+
 int main(int argc, char** argv)
 {
-	std::string windowName = "Video";
-	cv::namedWindow(windowName);
+	std::string windowName = "prout";
+	bool BnWInputs = true;
 
 	// Chargement des images
 	VideoCapture video("./resources/set1/video.mp4");
-	Mat imageToTrack = imread("./resources/set1/naruto.jpg", 1);
+	Mat targeFile = imread("./resources/set1/naruto.jpg", 1);
+	Mat imageToTrack;
+	cv::cvtColor(targeFile, imageToTrack, COLOR_BGR2GRAY);
 
 	if (!video.isOpened() && !imageToTrack.empty()) // On test si notre vidéo est lue correctement
 	{
@@ -28,13 +31,22 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
-	cv::Ptr<cv::ORB> orb = ORB::create();
+	cv::Ptr<cv::ORB> orb = ORB::create();//team classic shit
+
 	Mat currentFrame;
 
 	while (true) {
 		if (!video.read(currentFrame))break;
-		CVengine.detectComputePoints(orb, &currentFrame, &imageToTrack);
-		CVengine.drawWindow(&currentFrame, &imageToTrack, windowName);
+		Mat frame;
+		cv::cvtColor(currentFrame, frame, COLOR_BGR2GRAY);
+		if (BnWInputs) {
+			CVengine.detectComputePoints(orb, &frame, &imageToTrack);
+			CVengine.drawWindow(&frame, &imageToTrack, windowName);
+		}
+		else {
+			CVengine.detectComputePoints(orb, &currentFrame, &targeFile);
+			CVengine.drawWindow(&currentFrame, &targeFile, windowName);
+		}
 		if (cv::waitKey(16) == 27) {
 			break;
 		}
